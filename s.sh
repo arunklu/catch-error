85a2965803568d5c2c3f25bfd77e3356d3f2dfd4
arunklu <131229647+arunklu@users.noreply.github.com>
2023-05-30 17:16:43 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -1,3 +1,3 @@
 MYSQL_USERNAME="admin"
 MYSQL_PASSWORD="password"
-mysql -u "$MYSQL_USERNAME" -p"$MYSQL_PASSWORD" -h deliveryguru.cvsned4gvlis.ap-south-1.rds.amazonaws.com
+mysql -u "$MYSQL_USERNAME" -p"$MYSQL_PASSWORD" -h test-db.cvsned4gvlis.ap-south-1.rds.amazonaws.com
